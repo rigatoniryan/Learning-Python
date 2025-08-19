75fb4ab6485596aba198204f8a26a3f7e4f564ed
rigatoniryan <ryanta22@gmail.com>
2025-08-18 17:18:17 -0700

    luffy class test

---
+++ Udemy Course/classTutorial.py
@@ -0,0 +1,14 @@
+class Luffy(): 
+    
+    #Class Object Attribute
+    #Same for any instance of a class
+    devilFruit = 'Gomu Gomu no Mi'
+
+    #Init method for user defined attributes
+    #methods are functions defined inside a class
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def gumgumpistol(self):
+        print("Gum-Gum Pistol!")
---
+++ Udemy Course/classTutorialTester.py
@@ -0,0 +1,8 @@
+import classTutorial
+
+luffy = classTutorial.Luffy("Monkey D. Luffy", 19)
+
+print(luffy.name)
+print(luffy.age)
+print(luffy.devilFruit)
+luffy.gumgumpistol()

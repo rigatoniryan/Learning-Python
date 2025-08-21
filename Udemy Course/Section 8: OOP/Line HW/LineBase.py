b53bbe9928b15fad187b1751425b514159c92ee9
rigatoniryan <ryanta22@gmail.com>
2025-08-20 18:27:56 -0700

    line

---
+++ Udemy Course/Section 8: OOP/Line HW/LineBase.py
@@ -0,0 +1,18 @@
+class Line():
+
+    #the coords will be tuples
+    def __init__(self, coord1, coord2):
+        self.coord1 = coord1
+        self.coord2 = coord2   
+
+    def distance(self):
+        x1, y1 = self.coord1
+        x2, y2 = self.coord2
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+    def slope(self):
+        x1, y1 = self.coord1
+        x2, y2 = self.coord2
+        if x2 - x1 == 0:
+            return None  # Undefined slope
+        return (y2 - y1) / (x2 - x1)
---
+++ Udemy Course/Section 8: OOP/Line HW/LineTester.py
@@ -0,0 +1,5 @@
+import LineBase
+
+line = LineBase.Line((1, 2), (3, 4))
+print(line.distance())
+print(line.slope())
